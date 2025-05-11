e6b7cfddff656805bb1e11541c1ff6b81253a327
Johann Lizana <johann.lizana@alumnos.ucn.cl>
2025-05-11 14:38:53 -0400

    feat(breeds): agregar archivo de razas de perros con imágenes y nombres

---
+++ src/data/breeds.ts
@@ -0,0 +1,27 @@
+export const breeds = [
+    {
+      name: 'Mixed Breed',
+      image: require('../assets/breeds/mixed_breed.png'),
+    },
+    {
+      name: 'Afghan Hound',
+      image: require('../assets/breeds/afghan_hound.png'),
+    },
+    {
+      name: 'Golden Retriever',
+      image: require('../assets/breeds/golden.png'),
+    },
+    {
+      name: 'Akita',
+      image: require('../assets/breeds/akita.png'),
+    },
+    {
+      name: 'Border Collie',
+      image: require('../assets/breeds/border_collie.png'),
+    },
+    {
+      name: 'Dalmatian',
+      image: require('../assets/breeds/dalmatian.png'),
+    },
+  ];
+  
