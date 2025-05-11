fa8be550b534e8b8a28ac0b1d887420e1333e5bc
Johann Lizana <johann.lizana@alumnos.ucn.cl>
2025-05-11 11:46:50 -0400

    feat(pet_model): agregar interfaz del modelo de mascota para definir sus atributos

---
+++ src/models/pet_model.ts
@@ -0,0 +1,13 @@
+export interface pet_model {
+    id: number;
+    name: string;
+    breed: string;
+    age: number;
+    zone: string;
+    description: string;
+    comments: string;
+    medical_condition: string;
+    photo: string;
+    user_id: number;
+  }
+  
