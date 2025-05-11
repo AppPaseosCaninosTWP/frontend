42cc54182219180b9e37f88210dbbdff7781223e
Johann Lizana <johann.lizana@alumnos.ucn.cl>
2025-05-11 15:21:29 -0400

    feat(app): envolver NavigationContainer con PetCreationProvider para gestión de contexto

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/stack_navigator';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import { PetCreationProvider } from './src/context/pet_creation_context'; // ✅ importa el contexto
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <PetCreationProvider> { }
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </PetCreationProvider>
     </GestureHandlerRootView>
   );
 }
-
-
