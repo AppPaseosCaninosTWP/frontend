4179f06ddf22ffbb671115b6091b6d524b917f38
Johann Lizana <johann.lizana@alumnos.ucn.cl>
2025-05-11 00:53:03 -0400

    feat(user_model): agregar campo role_name a la interfaz del modelo de usuario

--- src/models/user_model.ts
+++ src/models/user_model.ts
@@ -1,9 +1,10 @@
 //user_model.ts
 
 export interface user_model {
-    id: number;
-    email: string;
-    phone: string;
-    role_id: number;
-    is_active: boolean;
-  }
+  id: number;
+  email: string;
+  phone: string;
+  role_id: number;
+  role_name: string;
+  is_active: boolean;
+}
