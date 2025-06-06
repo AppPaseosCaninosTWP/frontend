f7956091f8198450d29be5df582451d520ffc1a7
Andres Alejandro Almeyda Bustamante <148406092+AndresAlmeyda0@users.noreply.github.com>
2025-06-06 15:47:04 -0400

    feat(walk_model): Se agregaron modelos usados en la screen de walks_screen del admin

--- src/models/walk_model.ts
+++ src/models/walk_model.ts
@@ -30,3 +30,72 @@ export interface create_walk_payload {
   duration: number;
   days: string[];
 }
+
+export interface APIWalkFromList {
+  walk_id: number;
+  walk_type: string;
+  status: string;
+  client_email: string;
+  walker_email: string | null;
+  days: {
+    start_date: string;
+    start_time: string;
+    duration: number;
+  }[];
+  pets: {
+    pet_id: number;
+    name: string;
+    photo: string;
+    zone: string;
+  }[] | null;
+}
+
+export interface APIWalkFromDetail {
+  walk_id: number;
+  walk_type:
+    | string
+    | {
+        walk_type_id: number;
+        name: string;
+      };
+  status: string;
+  comments: string | null;
+  client: {
+    user_id: number;
+    email: string;
+    phone: string;
+  };
+  walker:
+    | {
+        user_id: number;
+        email: string;
+        phone: string;
+      }
+    | null;
+  pets: {
+    pet_id: number;
+    name: string;
+    photo: string;
+    zone: string;
+  }[] | null;
+  days: {
+    start_date: string;
+    start_time: string;
+    duration: number;
+  }[];
+}
+
+export interface DetailedWalk {
+  id: string;
+  clientName: string;
+  walkerName: string;
+  walkerAvatar: any;
+  petNames: string;
+  zone: string;
+  date: string;
+  status: 'Pendiente' | 'Confirmado' | 'En curso' | 'Finalizado' | 'Cancelado';
+  startTime: string;
+  endTime: string;
+  type: string;
+  notes: string;
+}
